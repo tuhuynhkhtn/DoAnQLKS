8b6c7052ad8edd0a006216c4663d487537f32458
tuhuynhkhtn <tuhuynhkhtn@gmail.com>
2018-02-20 16:13:39 +0700

    LoadRoom
    LoadRoom

---
+++ 1.1. LoadRoom/MVCQLKS/MVCQLKS/Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using MVCQLKS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCQLKS.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        public ActionResult GetList()
+        {
+            using (var dc = new QLKSEntities())
+            {
+                var l = dc.Categories.ToList();
+                return PartialView("_PartialList", l);
+            }
+        }
+    }
+}
