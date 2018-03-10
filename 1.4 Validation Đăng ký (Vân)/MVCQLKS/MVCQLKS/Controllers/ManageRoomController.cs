b6d8137c2e63a7df4f0cb81047e67526a317f393
tuhuynhkhtn <tuhuynhkhtn@gmail.com>
2018-03-10 15:21:28 +0700

    1.4 Validation Đăng ký (Vân)
    1.4 Validation Đăng ký (Vân)

---
+++ 1.4 Validation Đăng ký (Vân)/MVCQLKS/MVCQLKS/Controllers/ManageRoomController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCQLKS.Models;
+
+namespace MVCQLKS.Controllers
+{
+    public class ManageRoomController : Controller
+    {
+        // GET: ManageRoom
+        public ActionResult IndexManageRoom()
+        {
+            using (var dc = new QLKSEntities())
+            {
+                var l = dc.Rooms.ToList();
+                return View(l);
+            }
+        }
+
+        // GET: ManageCategory
+        public ActionResult IndexManageCategory()
+        {
+            using (var dc = new QLKSEntities())
+            {
+                var l = dc.Categories.ToList();
+                return View(l);
+            }
+        }
+    }
+}
---
+++ 1.4 Validation Đăng ký (Vân)/MVCQLKS/MVCQLKS/Ultilities/Ulti.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace MVCQLKS.Ultilities
+{
+    public class Ulti
+    {
+        public static string Md5Hash(string str)
+        {
+            using (var md5 = MD5.Create())
+            {
+                byte[] arr = Encoding.ASCII.GetBytes(str);
+                byte[] arrMd5 = md5.ComputeHash(arr);
+                StringBuilder sb = new StringBuilder();
+                foreach(var b in arrMd5)
+                {
+                    sb.AppendFormat("{0:X}", b);
+                }
+                return sb.ToString();
+            }
+        }
+    }
+}
