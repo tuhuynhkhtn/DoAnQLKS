7c849d704ba0233f4432a4b706797a2f0ff168d0
tuhuynhkhtn <tuhuynhkhtn@gmail.com>
2018-03-10 15:19:02 +0700

    1.3 Tạo Layout Đăng kí (Vân)
    1.3 Tạo Layout Đăng kí (Vân)

---
+++ 1.3 Tạo Layout Đăng kí(Vân)/MVCQLKS/MVCQLKS/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using MVCQLKS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCQLKS.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Register(User user)
+        {
+            return View();
+        }
+    }
+}
---
+++ 1.3 Tạo Layout Đăng kí(Vân)/MVCQLKS/MVCQLKS/Controllers/RoomController.cs
@@ -0,0 +1,22 @@
+﻿using MVCQLKS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCQLKS.Controllers
+{
+    public class RoomController : Controller
+    {
+        // GET: Room
+        public ActionResult GetListByCategory(int catId)
+        {
+            using (var dc = new QLKSEntities())
+            {
+                var l = dc.Rooms.Where(p => p.CatID == catId).ToList();
+                return View("ListByCategory", l);
+            }
+        }
+    }
+}
